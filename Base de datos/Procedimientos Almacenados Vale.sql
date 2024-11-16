21c67af22f8b8a4b4323cecd3a8803b6834f4ba2
Vale-Torres05 <valetorresarce@gmail.com>
2024-11-15 23:48:01 -0600

    SP CRUD de inventario

--- Base de datos/Procedimientos Almacenados Vale.sql
+++ Base de datos/Procedimientos Almacenados Vale.sql
@@ -53,3 +53,55 @@ BEGIN
     DELETE FROM ENVIOS WHERE ID_ENVIO = p_id_envio;
 END sp_delete_envio;
 /
+
+//Crear inventario
+CREATE OR REPLACE PROCEDURE sp_create_inventario (
+    p_id_sucursal NUMBER,
+    p_id_producto NUMBER,
+    p_cantidad NUMBER
+) AS
+BEGIN
+    INSERT INTO INVENTARIO (ID_SUCURSAL, ID_PRODUCTO, CANTIDAD)
+    VALUES (p_id_sucursal, p_id_producto, p_cantidad);
+END sp_create_inventario;
+/
+
+//Obtener inventario
+CREATE OR REPLACE PROCEDURE sp_get_inventario (
+    p_id_inventario NUMBER,
+    p_id_sucursal OUT NUMBER,
+    p_id_producto OUT NUMBER,
+    p_cantidad OUT NUMBER,
+    p_ultima_actualizacion OUT DATE
+) AS
+BEGIN
+    SELECT ID_SUCURSAL, ID_PRODUCTO, CANTIDAD, ULTIMA_ACTUALIZACION
+    INTO p_id_sucursal, p_id_producto, p_cantidad, p_ultima_actualizacion
+    FROM INVENTARIO
+    WHERE ID_INVENTARIO = p_id_inventario;
+END sp_get_inventario;
+/
+
+//Actualizar inventario
+CREATE OR REPLACE PROCEDURE sp_update_inventario (
+    p_id_inventario NUMBER,
+    p_id_sucursal NUMBER,
+    p_id_producto NUMBER,
+    p_cantidad NUMBER
+) AS
+BEGIN
+    UPDATE INVENTARIO
+    SET ID_SUCURSAL = p_id_sucursal, ID_PRODUCTO = p_id_producto, CANTIDAD = p_cantidad,
+        ULTIMA_ACTUALIZACION = SYSDATE
+    WHERE ID_INVENTARIO = p_id_inventario;
+END sp_update_inventario;
+/
+
+//Eliminar inventario
+CREATE OR REPLACE PROCEDURE sp_delete_inventario (
+    p_id_inventario NUMBER
+) AS
+BEGIN
+    DELETE FROM INVENTARIO WHERE ID_INVENTARIO = p_id_inventario;
+END sp_delete_inventario;
+/
